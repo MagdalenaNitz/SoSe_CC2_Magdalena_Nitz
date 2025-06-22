d89d6bda99c210aaa49851af7a5c68573ced35cd
MagdalenaNitz <magdalenanitz@gmx.de>
2025-06-22 14:24:45 +0200

    .

--- Natur/pond.js
+++ Natur/pond.js
@@ -3,13 +3,14 @@ AFRAME.registerComponent('pond', {
     const pond = document.createElement('a-cylinder');
     pond.setAttribute('id', 'pond');
     pond.setAttribute('radius', '1.5');
-    pond.setAttribute('height', '0.2');              // Tiefe des Teichs
-    pond.setAttribute('position', '0 0 -5');         // Oberkante bündig mit Boden
+    pond.setAttribute('height', '0.2'); // Mehr Tiefe
+    pond.setAttribute('position', '0 -0.02 -5');
     pond.setAttribute('rotation', '0 0 0');
-    pond.setAttribute('class', 'clickable');
     pond.setAttribute('src', '#waterTex');
-    pond.setAttribute('material', 'shader: standard; transparent: true; opacity: 0.6; metalness: 0.1; roughness: 0.3; side: double;');
+    pond.setAttribute('class', 'clickable');
+    pond.setAttribute('material', 'shader: standard; transparent: true; opacity: 0.6; roughness: 0.3; metalness: 0.1; side: double');
 
     this.el.appendChild(pond);
   }
 });
+
