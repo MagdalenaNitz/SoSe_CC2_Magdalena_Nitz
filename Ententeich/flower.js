1720ab7eaeb01875022d256cfe76081b4288c32b
MagdalenaNitz <magdalenanitz@gmx.de>
2025-06-27 13:34:49 +0200

    .

--- Ententeich/flower.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const flowers = document.querySelectorAll('.flower');
-    flowers.forEach(flower => {
-      flower.addEventListener('click', () => {
-        window.dispatchEvent(new CustomEvent('flower-clicked'));
-      });
-    });
-  });
-  
